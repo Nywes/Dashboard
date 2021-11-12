5b5febd3954f8bf13ce6788068a5cab01d93927a
Gabriel <gabriel.griffin@epitech.eu>
2021-11-12 14:10:25 +0100

    server/models - U: added a user model (unique id seems iffy)

---
+++ server/models/user-model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// ! hash user password obviously
+// ! md5 ? SHA-256 ?
+// ! store the decryption key in your secrets / ENV
+const User = new Schema(
+    {
+        name: { type: String, required: true },
+        password: { type: String, required: true },
+        unique_id: { type: Number, required: true, unique: true, dropDups: true }
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('users', User)
