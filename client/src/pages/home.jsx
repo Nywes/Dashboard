aab696f828025523be73d1b852eeefc9ef66eb26
Nywes <Nywes@localhost.localdomain>
2021-11-22 17:46:58 +0100

    widgetManager -A the hide and seek function is now working

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,31 +1,64 @@
 import React, { Component } from 'react'
 import { Wrapper, Container, Item, WidgetManager } from "../style";
 import { LogoButton } from '../components/HomeHeader';
-import { AccountCircle, Widgets, Visibility } from '@mui/icons-material';
+import { AccountCircle, Widgets, Visibility, VisibilityOff } from '@mui/icons-material';
 import styles from "../style/HomePage.module.css";
+
+class WidgetInterface extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isManager: this.props.isManager,
+            display: true,
+        }
+    }
+    handlechange = () =>
+    {
+        this.setState({display:!this.state.display})
+    }
+
+    render() {
+        let seekManager = <WidgetManager onClick={this.handlechange}><Visibility className={styles.VisibilityIcon}/></WidgetManager>;
+        let hiddenManager = <WidgetManager onClick={this.handlechange}><VisibilityOff className={styles.VisibilityIcon}/></WidgetManager>;
+        let seekWidget = <Item/>;
+
+        let temp = this.props.isManager;
+        let tempDisplay = this.state.display;
+
+        console.log(this.state.display);
+        if (temp) {
+            if (tempDisplay) {
+                return (seekWidget);
+            } else {
+                return (null);
+            }
+        } else {
+            if (tempDisplay) {
+                return (seekManager);
+            } else {
+                return (hiddenManager);
+            }
+        }
+    }
+}
 
 class Home extends Component {
     constructor({props}) {
-        super(props)
+        super(props);
         this.state = {
-            display: true,
-            name: ''
+            name: '',
+            divcont: true,
         }
-    }
-
-    state = {
-        divcont: true,
     }
     // * basically an "init" function
     componentDidMount = async () => {
     }
 
     render() {
-        var handlechange = e =>
+        var handlechange = () =>
         {
             this.setState({divcont:!this.state.divcont})
         }
-        const x=this.state.divcont;
         return (
             <Wrapper className="App">
                 <header className={styles.HeaderHomePage}>
@@ -33,15 +66,15 @@ class Home extends Component {
                     <LogoButton path={ '/login' } icon={AccountCircle}/>
                 </header>
                 <Container>
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
-                    {x?<Item/>:<WidgetManager><Visibility className={styles.VisibilityIcon}/></WidgetManager>}
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
+                    <WidgetInterface isManager={this.state.divcont}/>
                 </Container>
             </Wrapper>
         )
