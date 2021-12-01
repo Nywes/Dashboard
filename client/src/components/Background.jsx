074e8c5fc82e6148bb6ecabb34c082e91e449c82
Gabriel <gabriel.griffin@epitech.eu>
2021-12-01 15:50:48 +0100

    client/ - U: used callbacks to change background image (warning, the bigger the image, the longer the load time)

--- client/src/components/Background.jsx
+++ client/src/components/Background.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import image from "../img/desert.jpg";
-import {background} from "../globals/globalsVariables.js";
+import {background, SetCallback} from "../globals/globalsVariables.js";
 
 class Background extends Component {
     constructor(props) {
         super(props);
+
+        SetCallback(newBG => {
+            this.setState({
+                backgroundImage: newBG
+            })
+        })
 
         this.state = {
             backgroundImage: this.props.background
--- client/src/globals/globalsVariables.js
+++ client/src/globals/globalsVariables.js
@@ -1,13 +1,25 @@
 var background = "https://images6.alphacoders.com/352/352456.jpg";
 
+var callBack = null;
+
+function SetCallback(newCallback)
+{
+    callBack = newCallback;
+}
+
 function SetBackground(NewSrc)
 {
     background = NewSrc;
     console.log("New background", background);
+
+    if (callBack != null) {
+        callBack(background);
+    }
 }
 
 module.exports = {
     background,
-    SetBackground
+    SetBackground,
+    SetCallback
 }
 //export default { background, SetBackground }
